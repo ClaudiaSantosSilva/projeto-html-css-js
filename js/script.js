6feb3126ab87ee70844210c59d2a0698c1eb0510
claudia.tss <claudia.tss@outlook.com>
2023-04-11 18:14:34 -0300

    Botão Enviar do form concatenando strings no alert

--- js/script.js
+++ js/script.js
@@ -1,38 +1,57 @@
 //inicio conteudo 2
-let MostrarDivTexto=document.getElementById("tema-atual-1");
+let mostrarDivTexto=document.getElementById("tema-atual-1");
 let divTexto=document.querySelector("#texto-tema-atual-1");
+let temaAtualTituloUm=document.querySelector ("#tema-atual-titulo-1");
 
-MostrarDivTexto.addEventListener('click', ()=>{
+mostrarDivTexto.addEventListener('click', ()=>{
     if (divTexto.style.display==='none'){
         divTexto.style.display='block';
+        temaAtualTituloUm.style.color='red';
     } else{
         divTexto.style.display='none';
+        temaAtualTituloUm.style.color='black';
     }
 })
 
-let MostrarDivTextoDois=document.getElementById("tema-atual-2");
+let mostrarDivTextoDois=document.getElementById("tema-atual-2");
 let divTextoDois=document.querySelector("#texto-tema-atual-2");
+let temaAtualTituloDois=document.querySelector ("#tema-atual-titulo-2");
 
-MostrarDivTextoDois.addEventListener('click', ()=>{
+mostrarDivTextoDois.addEventListener('click', ()=>{
     if (divTextoDois.style.display==='none'){
         divTextoDois.style.display='block';
+        temaAtualTituloDois.style.color='red';
     } else{
         divTextoDois.style.display='none';
+        temaAtualTituloDois.style.color='black';
     }
 })
 
-let MostrarDivTextoTres=document.getElementById("tema-atual-3");
+let mostrarDivTextoTres=document.getElementById("tema-atual-3");
 let divTextoTres=document.querySelector("#texto-tema-atual-3");
+let temaAtualTituloTres=document.querySelector ("#tema-atual-titulo-3");
 
-MostrarDivTextoTres.addEventListener('click', ()=>{
+mostrarDivTextoTres.addEventListener('click', ()=>{
     if (divTextoTres.style.display==='none'){
         divTextoTres.style.display='block';
+        temaAtualTituloTres.style.color='red';
     } else{
         divTextoTres.style.display='none';
+        temaAtualTituloTres.style.color='black';
     }
 })
 
 //fim conteudo 2
+//inicio formulário
+let botaoEnviar=document.getElementById("form-sugestao");
+let nome=document.getElementById("form-nome");
+
+botaoEnviar.addEventListener("submit", (event) => {
+        event.preventDefault();
+        alert(nome.value + ", obrigada pela sugestão! ");
+});
+
+//fim formulário
 
 
 /*const albumGrid=document.querySelector("#album-grid");
