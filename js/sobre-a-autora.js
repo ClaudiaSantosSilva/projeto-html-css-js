81f13a2ba5fd34012b930dfe03a5811c313380b5
claudia.tss <claudia.tss@outlook.com>
2023-04-11 21:48:51 -0300

    pg sobre autora, criada li no HTML via JS

---
+++ js/sobre-a-autora.js
@@ -0,0 +1,11 @@
+let listaFormacao=document.getElementById("formacao");
+let formacoes=[
+    "Graduada em Nutrição pela UERJ",
+    "Especialista em Nutrição Clínica pela UFF",
+    "Residência em Suporte Nutricional pelo HUPE/UERJ",
+    "Mestre em Nutrição Clínica pela Universidade do Porto",
+    "Especialista em Gerontologia pela UnATI/UERJ",
+    "Especialista em Oncologia pela SBNO",
+]
+
+listaFormacao.innerHTML= formacoes.map((f)=>"<li>"+f+"</li>").join("");
