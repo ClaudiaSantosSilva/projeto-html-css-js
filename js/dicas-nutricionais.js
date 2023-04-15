e659cc66cb268c14b3039c3e4253c67def0036f0
claudia.tss <claudia.tss@outlook.com>
2023-04-14 22:02:51 -0300

    dicas nutricionais com JS e responsividade

--- js/dicas-nutricionais.js
+++ js/dicas-nutricionais.js
@@ -1,35 +1,72 @@
 let dicasNutricionais=[
     {
-      imagem:img src="" ,
-      titulo: "Como aumentar o consumo de ferro",
-      texto: "",
+      createdAt: "Há 1 dia", 
+      imageSrc:"imagens/alimentos-fonte-ferro.webp" ,
+      titulo: "Como aumentar a absorção do ferro dos alimentos",
+      texto: "Consuma fruta cítrica durante o almoço e o jantar (suco) ou como sobremesa. Evite bebidas como mate, coca-cola e café durante essas refeiçoes.",
     },
 
     {
-        imagem = img src="",
-        titulo = "",
-        texto = "",
+        createdAt: "Há uma semana",
+        imageSrc:"imagens/beterraba.jpg",
+        titulo : "A beterraba e o paciente diabético",
+        texto : "Ao contrário do que muitos dizem, a beterraba não tem açúcar (como aquele usado nas bebidas). Ela pode e deve ser usada como parte de uma alimentação variada e saudável.",
     },
 
     {
-        imagem = img src="",
-        titulo = "",
-        texto = "",
+        createdAt: "Há duas semanas",
+        imageSrc:"imagens/alimentacao-e-exercicio.jpg",
+        titulo : "Perda de peso = alimentação + exercício + consciência",
+        texto : "Para que a perda de peso seja saudável e duradoura, a alimentação e o exercicio devem fazer parte da rotina diária, de forma consciente.",
     },
 ];
 
-{ 
+let dicasNutricionaisList=document.querySelector('#dicas-nutricionais-list');
+dicasNutricionais.forEach((dicaData) =>{
+    const dicaItem=getDicaItem(
+        dicaData.createdAt,
+        dicaData.imageSrc,
+        dicaData.titulo,
+        dicaData.texto
+    );
 
-    //exemplos do Felipe
-    { 
-        imagem: "alimentos-saudaveis.jpg",
-        titulo: "ASDASD",
-        text: "ASDASODAS"
-        }
+    dicasNutricionaisList.appendChild(dicaItem);
+    });
 
+    function getDicaItem(createdAt,imageSrc,titulo,texto){
+        let dicasNutricionaisListItem=document.createElement('article');
+        dicasNutricionaisListItem.className='dicas-nutricionais-list-item';
 
-        src='imagens/${nomeDaImagem'
-<img src='imagens/${nomeDaImagem' />
+        let dicasNutricionaisListItemInfo=document.createElement('div');
+        dicasNutricionaisListItemInfo.className='dicas-nutricionais-list-item-info';
 
-document.getElementById("myImg").src = "hackanm.gif";
-document.getElementById("myImg").src = ${dicasNutricionais.imagem};
+        let dicasNutricionaisListItemInfoContent=document.createElement('div');
+        dicasNutricionaisListItemInfoContent.className='dicas-nutricionais-list-item-info-content';
+
+        let dicasNutricionaisListItemCreatedAt=document.createElement('span');
+        dicasNutricionaisListItemCreatedAt.className='dicas-nutricionais-list-item-created-at';
+        dicasNutricionaisListItemCreatedAt.innerHTML=createdAt;
+
+        let dicasNutricionaisListItemImageSrc=document.createElement('img');
+        dicasNutricionaisListItemImageSrc.className='dicas-nutricionais-list-item-image-src';
+        dicasNutricionaisListItemImageSrc.src=imageSrc;
+
+        let dicasNutricionaisListItemTitulo=document.createElement('h3');
+        dicasNutricionaisListItemTitulo.className='dicas-nutricionais-list-item-titulo';
+        dicasNutricionaisListItemTitulo.innerHTML=titulo;
+
+        let dicasNutricionaisListItemTexto=document.createElement('p');
+        dicasNutricionaisListItemTexto.className='dicas-nutricionais-list-item-texto';
+        dicasNutricionaisListItemTexto.innerHTML=texto;
+
+        dicasNutricionaisListItemInfo.appendChild(dicasNutricionaisListItemImageSrc);
+        dicasNutricionaisListItemInfoContent.appendChild(dicasNutricionaisListItemCreatedAt);
+        dicasNutricionaisListItemInfo.appendChild(dicasNutricionaisListItemInfoContent);
+
+        dicasNutricionaisListItem.appendChild(dicasNutricionaisListItemInfo);
+        dicasNutricionaisListItem.appendChild(dicasNutricionaisListItemTitulo);
+        dicasNutricionaisListItem.appendChild(dicasNutricionaisListItemTexto);
+
+        return dicasNutricionaisListItem;
+
+    }
